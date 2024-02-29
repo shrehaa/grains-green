5a3b5741075d7425622a72e5de807ceaf3470852
Chowdhury <schowdhu@bechtel.com>
2024-02-29 17:27:52 +0530

    foodapp first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./componets/navbar/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
